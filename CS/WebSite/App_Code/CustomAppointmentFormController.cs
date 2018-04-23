c863359f6c58a01b9bdf299c008fe4cc35ba1045
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/CustomAppointmentFormController.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Web.ASPxScheduler;
+using DevExpress.Web.ASPxScheduler.Internal;
+using DevExpress.XtraScheduler;
+
+
+public class CustomAppointmentFormController : AppointmentFormController {
+    public CustomAppointmentFormController(ASPxScheduler control, Appointment apt)
+        : base(control, apt) { }
+
+    public string CustomInfoField {
+        get { return (string)EditedAppointmentCopy.CustomFields["ApptCustomInfo"]; }
+        set { EditedAppointmentCopy.CustomFields["ApptCustomInfo"] = value; }
+    }
+
+    string SourceCustomInfoField {
+        get { return (string)SourceAppointment.CustomFields["ApptCustomInfo"]; }
+        set { SourceAppointment.CustomFields["ApptCustomInfo"] = value; }
+    }
+
+    public override bool IsAppointmentChanged() {
+        bool isChanged = base.IsAppointmentChanged();
+        return isChanged || SourceCustomInfoField != CustomInfoField;
+    }
+}
---
+++ CS/WebSite/App_Code/CustomAppointmentSaveCallbackCommand.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Web;
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.Web.ASPxScheduler.Internal;
+using System.Web.UI;
+
+
+public class CustomAppointmentSaveCallbackCommand : AppointmentFormSaveCallbackCommand {
+    public CustomAppointmentSaveCallbackCommand(ASPxScheduler control) : base(control) { }
+
+    protected internal new CustomAppointmentFormController Controller {
+        get { return (CustomAppointmentFormController)base.Controller; }
+    }
+
+    protected override AppointmentFormController CreateAppointmentFormController(DevExpress.XtraScheduler.Appointment apt) {
+        return new CustomAppointmentFormController(Control, apt);
+    }
+
+    protected override Control FindControlByID(string id) {
+        return FindTemplateControl(TemplateContainer, id);
+    }
+
+    System.Web.UI.Control FindTemplateControl(System.Web.UI.Control RootControl, string id) {
+        System.Web.UI.Control foundedControl = RootControl.FindControl(id);
+        if (foundedControl == null) {
+            foreach (System.Web.UI.Control item in RootControl.Controls) {
+                foundedControl = FindTemplateControl(item, id);
+                if (foundedControl != null) break;
+            }
+        }
+        return foundedControl;
+    }
+
+    protected override void AssignControllerValues() {
+        base.AssignControllerValues();
+
+        ASPxComboBox cbSubject = FindControlByID("cbSubject") as ASPxComboBox;
+        ASPxTextBox tbCustomInfo = FindControlByID("tbCustomInfo") as ASPxTextBox;
+
+        Controller.Subject = cbSubject.Text;
+        Controller.CustomInfoField = tbCustomInfo.Text;
+    }
+}
